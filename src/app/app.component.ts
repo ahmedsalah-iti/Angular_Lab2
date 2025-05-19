bc010cf2526149747b72b877c5b459f77d81a376
Ahmed Salah <ahmedsalah.iti@gmail.com>
2025-05-19 16:00:03 +0300

    almost fininshed lab2 task123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TracksComponentComponent } from './tracks-component/tracks-component.component';
 import { TodoComponent } from './todo/todo.component';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TracksComponentComponent,TodoComponent],
+  imports: [ProductComponent,TracksComponentComponent,TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
